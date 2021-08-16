df633641cb4cba8bfea6464b6964bb1cf2f69dd3
encryted-chap <Encrypted-Chap@protonmail.com>
2021-08-16 18:24:18 -0400

    the identify thingy finally works :D

--- PenisOS/Drivers/ATA.hpp
+++ PenisOS/Drivers/ATA.hpp
@@ -10,24 +10,40 @@
 class ATA_Bus {
 public:
     ATA_Bus() {
-        // switch to first bus
-        IO_Base = ATA_IO::Primary_IO;
-        CTRL_Base = ATA_CTRL::Primary_C;
-        
-        // identify and activate the drives
-        Master = Identify(Drive::DriveType::Master);
-        Slave = Identify(Drive::DriveType::Slave);
-        
-    }
-    // flushes the cache on the currently selected ATA bus
-    void FlushCache() {
-        // send cache flush cmd to command register
-        outb(IO_Base+7, 0xE7);
-
-        // wait for BSY to clear
-        while(GetBit(inb(CTRL_Base+0), 7)); 
+        busIndex = -1; // make sure bus index is able to iterate
+        SwitchBus(); // switches to next working bus
     }
 private:
+    // switches the current bus to the next bus
+    void SwitchBus() {
+        bool WorkingBus = false;
+        while(!WorkingBus) {
+            // iterate through to next bus offset
+            CTRL_Base = ATA_CTRL[++busIndex];
+            IO_Base = ATA_IO[busIndex];
+            if(inb(IO_Base+7) == 0xFF) { // read regular status byte for floating bus
+                // bus invalid, no drives connected
+                WorkingBus = false;
+                continue;
+            } else {
+                // continue to identify the drives connected
+                // Set drive types
+                Master.DType = Drive::Master;
+                Slave.DType = Drive::Slave;
+                // do identify drive operation to detect
+                Identify(&Master);
+                Identify(&Slave);
+
+                // check if neither working
+                if(!Master.Active && !Slave.Active) 
+                    continue; // switch to next bus
+                WorkingBus = true; // bus has a drive connected, cool?
+            }
+        }
+    }
+    
+    
+    int busIndex = 0; // used for controlling which bus is currently active
     // a structure representing a single drive
     struct Drive {
     public:
@@ -45,180 +61,69 @@ private:
         uint32_t Max28;
         uint64_t Max48;
         unsigned char slavebit = 8U;
+        void ParseIdentify() {
 
+        }
+        // sets the slavebit depending on the current drive type
         void SetSlavebit() {
             if(DType == Master) {
                 slavebit = 0U;
             } else slavebit = 8U; // 00001000 aka set the slavebit
         }
     };
-    Drive Master;
-    Drive Slave;
-    Drive Identify(Drive::DriveType Type) {
-        Drive Ret;
-        Ret.DType = Type;
+    Drive Master, Slave; // is this racist? i just follow the specs i don't make them :/
 
-        SelectDrive(Type);
-        ClearLBA(); // set LBA vals to 0
-        outb(IO_Base+7, 0xEC); // send IDENTIFY command
+    // identifies a drive, returning the information to the drives pointer
+    void Identify(Drive* toSelect) {
+        outb(IO_Base+6, toSelect->DType); // send drive type to drive select port
+        toSelect->SetSlavebit(); // make sure drive slavebit is correct
+        // 400ns delay:
+        uint8_t status;
+        for(int i = 0; i < 14; i++) {
+            status = inb(IO_Base+7); // read data for delay
+        }
+        // clear sectorcount-lbahi ports
+        for(int i = 0; i < 4; i++) {
+            int offset = i+2; // starts at 0x1F2, so offset = i+2
+            outb(IO_Base+offset, 0x00); // clear register
+        }
+        outb(IO_Base+7, 0xEC); // send identify command
 
-        // if value reads 0, drive does not exist
         if(inb(IO_Base+7) == 0) {
-            Ret.Active = false;
-            Ret.Supports48 = false;
-            Ret.Supports28 = false;
+            // drive does not exist
+            toSelect->Active = false;
         } else {
-            if(GetBit(inb(IO_Base+7), 0)) {
-                Ret.Active = true;
-                goto readvals;
-            }
-                
-            // poll
-            for(int i = 0; i < 9000; i++) {
-                if(!GetBit(inb(IO_Base+7), 7)) {
-                    Ret.Active = true;
-                    break;
-                }
-            }
-            if(!Ret.Active) {
-                // if these 2 ports are nonzero, this drive is not ATA compliant
+            // wait for BSY to clear
+            while(inb(IO_Base+7) == 8) {
+                // if nonzero dont poll
                 if(inb(IO_Base+4) && inb(IO_Base+5)) {
-                    Ret.Active = false;
-                    return Ret;
+                    toSelect->Active = false; // not ATA drive
+                    return;
+                }
+                if(inb(IO_Base+7) == 1) {
+                    // if ERR sets, quit
+                    toSelect->Active = false;
+                    return;
                 }
             }
-            // poll
-            while(true) {
-                // wait for DRQ or ERR to set
-                unsigned char status = inb(IO_Base+7);
-                if(GetBit(status, 3) || GetBit(status, 0)) {
-                    Ret.Active = true;
-                    break;
-                }
+            toSelect->Active = true; // this drive is working
+
+            // now we can read and return 256 uint16_t values
+            for(int i = 0; i < 256; i++) 
+                toSelect->identify_return[i] = inw(IO_Base);
+            for(int i = 0; i < 14; i++) {
+                inb(IO_Base+7); // 400ns delay (in case there's another select right after this one)s
             }
-        readvals:
-            uint16_t* iden;
-            // read the values
-            for(int i = 0; i < 256; i++) {
-                iden[i] = inw(IO_Base+0); // reads one value
-                
-            } 
-            NS_DELAY(); // give delay to allow for DRQ to reset
-            Ret.identify_return = iden;
-            Ret = ParseIdentify(Ret);
-            FlushCache();
-
-            return Ret;
+            toSelect->ParseIdentify(); // make sure to retrieve correct data
+            return; // end identify
         }
-    }
-    void NS_DELAY() {
-        byte current = inb(CTRL_Base+0);
-        for(int i = 0; i < 13; i++) {
-            if(inb(CTRL_Base+0) != current) 
-                return;
-        }
-    }
-    Drive ParseIdentify (Drive toParse) {
-        toParse.Supports48 = GetBit(toParse.identify_return[83], 10);
-        uint32_t max28 = (toParse.identify_return[61] >> 16) | toParse.identify_return[60];
-
-        uint32_t max48_lo = (toParse.identify_return[101] >> 16) | toParse.identify_return[100];
-        uint32_t max48_hi = (toParse.identify_return[103] >> 16) | toParse.identify_return[102];
-        uint64_t max48 = (max48_hi >> 32) | max48_lo;
-
-        if(!max28) 
-            toParse.Supports28 = false;
-        else {
-            toParse.Supports28 = true;
-            toParse.Max28 = max28;
-        }
-        if(toParse.Supports48) 
-            toParse.Max48 = max48;
         
-        uint8_t udmaSupported = (uint8_t)toParse.identify_return[88];
-        uint8_t udmaActive = (toParse.identify_return[88] << 8);
-        
-        // get UDMA for drive
-        for(int i = 0; i < 8; i++) {
-            if(GetBit(udmaActive, i))
-                toParse.CurrentUDMA = i;
-            if(GetBit(udmaSupported, i))
-                toParse.MaxUDMA = i;    
-        }
     }
-    // clears all the LBA ports
-    void ClearLBA() {
-        outb(IO_Base+2, 0);
-        outb(IO_Base+3, 0);
-        outb(IO_Base+4, 0);
-        outb(IO_Base+5, 0);
-    }
-    // selects a drive and returns the subsequent status register
-    uint8_t SelectDrive(unsigned char DriveID) {
-        uint8_t currentStatus = inb(CTRL_Base+0);
-        outb(IO_Base+6, DriveID);
-
-        // poll if value does NOT change in status reg
-        uint8_t updated = inb(CTRL_Base+0);
-        if(updated != currentStatus) {
-            return updated; // return new status
-        } else {
-            // waste 400ns for the thing to get its shit together
-            uint8_t Last = currentStatus; // read once
-
-            for(int i = 0; i < 12; i++) {
-                uint8_t current = inb(CTRL_Base+0);
-                // if this read is different, you can return it
-                if(current != Last) 
-                    return current;
-                else // otherwise, set last and continue
-                    current = Last;
-            } 
-            return inb(CTRL_Base+0); // return last value
-        }
-    }
-    // writes to a 28 bit LBA
-    uint16_t* Read28(uint32_t LBA, Drive drive, int SectorCount=1){
-        drive.SetSlavebit(); // make sure slavebit is accurate
-        if(drive.DType == Drive::Master) 
-            outb(IO_Base+6, 0xE0 |  (drive.slavebit << 4) | ((LBA >> 24) & 0x0F));
-        else 
-            outb(IO_Base+6, 0xF0 |  (drive.slavebit << 4) | ((LBA >> 24) & 0x0F));
-        outb(IO_Base+1, 0x00); // waste a tiny bit of cpu time
-        outb(IO_Base+2, (unsigned char)SectorCount); // send scount
-        outb(IO_Base+3, (unsigned char)LBA); // low 8 bits
-        outb(IO_Base+4, (unsigned char)(LBA >> 8)); // mid 8 bits
-        outb(IO_Base+5, (unsigned char)(LBA >> 16)); // high 8 bits
-
-        unsigned char current_status = inb(CTRL_Base+0); // get the status
-        // now to read
-        outb(IO_Base+7, 0x20); // READ SECTORS command
-
-        while(current_status == inb(CTRL_Base+0)); // poll
-        // now actually read all uint16_t's
-        uint16_t* ret;
-        for(int i = 0; i < 256*SectorCount; i++) {
-            ret[i] = inw(IO_Base+0);
-            
-            // if its about to hit a new sector, flush n' delay
-            if(!(i % 256) && i) {
-                FlushCache(); // prevent bad sectors
-                NS_DELAY(); // make sure to flush out old status
-            }
-        } NS_DELAY(); // give delay for resetting DRQ/BSY
-        return ret; // return the data buffer
-    }
-    enum ATA_CTRL {
-        Primary_C = 0x3F6,
-        Secondary_C = 0x376,
-        Tertiary_C = 0x3E6,
-        Quaternary_C = 0x366
+    uint8_t ATA_IO[4] {
+        0x1F0, 0x170, 0x1E8, 0x168
     };
-    enum ATA_IO {
-        Primary_IO = 0x1F0,
-        Secondary_IO = 0x170,
-        Tertiary_IO = 0x1E8,
-        Quaternary_IO = 0x168
+    uint8_t ATA_CTRL[4] {
+        0x3F6, 0x376, 0x3E6, 0x366
     };
     unsigned char IO_Base = 0x00;
     unsigned char CTRL_Base = 0x00;
--- PenisOS/Kernel.cpp
+++ PenisOS/Kernel.cpp
@@ -5,5 +5,6 @@ extern "C" void Entry();
 // the entry point of PenisOS
 // why am i doing this what the fuck
 void Entry() {
-    
+    ATA_Bus bus = ATA_Bus();
+    ((char*)0xb8000)[0] = 'h';
 }
